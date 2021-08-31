487131c7b101c6b929a251444ed654758d46d4bb
Adriana Castellanos A Jesús por María <davidsantiagocastellanos@gmail.com>
2021-08-31 17:39:59 -0500

    Listen test

--- scripts/status.sh
+++ scripts/status.sh
@@ -4,7 +4,7 @@ date +"%D-%R" >> i1Log
 echo lola
 curl 192.168.1.57:1111 >> i1Log
 date +"%D-%R" >> i2Log
-curl 192.168.1.67:1111 >> i2Log
+curl 192.168.1.71:1111 >> i2Log
 
 tail -1 i1Log
 tail -1 i2Log
