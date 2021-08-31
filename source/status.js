7b305a552896dd4020a05395e1641c0a73f53638
Adriana Castellanos A Jesús por María <davidsantiagocastellanos@gmail.com>
2021-08-31 17:53:30 -0500

    Listen test

--- source/status.js
+++ source/status.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec
 
 function getStatus(){
     var result
-    exec('./status.sh', function (err, stdout, stderr){
+    exec('./scripts/status.sh', function (err, stdout, stderr){
         result = stdout
         console.log("stdout"+stdout)
         console.log("err "+err)
