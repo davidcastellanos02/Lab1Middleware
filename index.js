eb01479c9a269b5f1e371defb1bb6204b97f2400
Adriana Castellanos A Jesús por María <davidsantiagocastellanos@gmail.com>
2021-08-31 16:35:19 -0500

    Listen test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const script = require('./scripts/status')
+const script = require('status')
 
 var instance1_status
 var instance2_status
--- status.js
+++ status.js
@@ -7,3 +7,5 @@ function getStatus(instance){
     })
     return result
 }
+
+//57
